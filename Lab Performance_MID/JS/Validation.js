aaeaa1ce703875af6e858acb76d1e6b9b3c6de77
prottoy94 <prottoyroy900@gmail.com>
2026-03-16 20:59:07 +0600

    JS file updated

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -3,3 +3,32 @@ console.log("Connected to Validation.js");
 const unitPrice=1000;
 const days=30;
 
+let qInput=document.getElementById("quantity");
+qInput.addEventListener("input",totalSum);
+
+function totalSum()
+{
+    let quantity=parseInt(document.getElementById("quantity").value);
+    if(quantity<0)
+    {
+        document.getElementById("quantityError").innerHTML="Quantity cannot be negative! Enter a valid quantity :((";
+        document.getElementById("quantity").value=0;
+        document.getElementById("totalPrice").value=0;
+        return;
+    }
+    else
+    {
+        document.getElementById("quantityError").innerHTML="";
+    }
+    if(isNaN(quantity))
+    {
+        quantity=0;
+    }
+
+    let total=unitPrice*quantity*days;
+    document.getElementById("totalPrice").value = total;
+
+    if(total>1000){
+        alert("Congratulations!!! You are eligible for a gift coupon :))");
+    }
+}
